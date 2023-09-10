5da32b28756114c95005e8f65f01b7fe54ebc062
manhhung2801 <hung28012002zz@gmail.com>
2023-09-10 15:03:01 +0700

    Templating and mark up

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="main-content mt-5">
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-6">
+                    <h4>Edit Post</h4>
+                </div>
+                <div class="col-md-6 d-flex justify-content-end">
+                    <a class="btn btn-success mx-1" href="">Back</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <form action="">
+                <div class="form-group">
+                    <label for="" class="form-label">Image</label>
+                    <input type="file" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="" class="form-label">Title</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="" class="form-label">Title</label>
+                    <select name="" id="" class="form-control">
+                        <option value="">test1</option>
+                        <option value="">test2</option>
+                        <option value="">test3</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="" class="form-label">Description</label>
+                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <div class="form-group mt-3">
+                    <button class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
