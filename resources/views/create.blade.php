045da2fd351c38ddd016c8384cac02357a45d90f
manhhung2801 <hung28012002zz@gmail.com>
2023-09-10 16:34:22 +0700

    creating form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <div class="main-content mt-5">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger">{{$error}}</div>
+            @endforeach
+        @endif
     <div class="card">
         <div class="card-header">
             <div class="row">
@@ -14,26 +19,28 @@
             </div>
         </div>
         <div class="card-body">
-            <form action="">
+            <form action="{{route('posts.store')}}" method="POST" enctype="multipart/form-data">
+                @csrf
                 <div class="form-group">
                     <label for="" class="form-label">Image</label>
-                    <input type="file" class="form-control">
+                    <input type="file" class="form-control" name="image">
                 </div>
                 <div class="form-group">
                     <label for="" class="form-label">Title</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" name="title">
                 </div>
                 <div class="form-group">
-                    <label for="" class="form-label">Title</label>
-                    <select name="" id="" class="form-control">
-                        <option value="">test1</option>
-                        <option value="">test2</option>
-                        <option value="">test3</option>
+                    <label for="" class="form-label">Category</label>
+                    <select id="" class="form-control" name="category_id">
+                        <option value="">Select</option>
+                        @foreach ($categories as $category)
+                            <option value="{{$category->id}}">{{$category->name}}</option>
+                        @endforeach
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="" class="form-label">Description</label>
-                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                    <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
                 </div>
                 <div class="form-group mt-3">
                     <button class="btn btn-primary">Submit</button>
