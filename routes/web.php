97b0ce9b18380954d2f1d469efd8490a9bb84a4c
manhhung2801 <hung28012002zz@gmail.com>
2023-09-19 15:23:28 +0700

    create Middleware via artisan command, global Middleware, Group middleware, Route Middleware, controller Middleware

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(["middleware" => "authCheck2"], function() {
+    Route::get('/dashboard', function() {
+        return view('dashboard');
+    });
+    Route::get('profile', function() {
+        return view('profile');
+    });
+});
+
 
 Route::get('/posts/trash', [PostController:: class, 'trashed'])->name('posts.trashed');
 Route::get('/posts/{id}/restore', [PostController::class, 'restore'])->name('posts.restore');
 Route::delete('/posts/{id}/force-delete', [PostController::class, 'forceDelete'])->name('posts.force_delete');
 
 Route::resource('posts',PostController:: class);
+
+Route::get('/unavailable', function() {
+    return view('unavailable');
+})->name('unavailable');
+
+// Route::group([], callback)
